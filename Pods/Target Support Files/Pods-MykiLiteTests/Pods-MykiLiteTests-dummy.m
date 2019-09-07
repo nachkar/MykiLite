8ffd9b74b7bd53041defea74b54a2c574f76e49b
noelachkar <noelachkar@hotmail.com>
2019-09-07 03:15:08 +0300

    gitignore fix

--- Pods/Target Support Files/Pods-MykiLiteTests/Pods-MykiLiteTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MykiLiteTests : NSObject
-@end
-@implementation PodsDummy_Pods_MykiLiteTests
-@end
--- Pods/Target Support Files/Pods-MykiLiteUITests/Pods-MykiLiteUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MykiLiteUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MykiLiteUITests
-@end
